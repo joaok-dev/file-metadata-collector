da8f28e573fe8c6e078e22415a8fd171e6d617bf
jkocx <joaoksoliveira1@gmail.com>
2023-09-22 18:15:06 -0300

    created variables related to the paths and saved the .csv file

--- scr/main.py
+++ scr/main.py
@@ -1,12 +1,29 @@
+import csv
 import os
 from datetime import datetime
 
-directory_path = os.curdir
-directory_contents = os.listdir(directory_path)
+# Get the directory of the current script
+current_script_directory = os.path.dirname(os.path.abspath(__file__))
+parent_directory = os.path.dirname(current_script_directory)
 
+# Define directory to scan and where to save the CSV
+directory_path = os.curdir
+save_directory = os.path.join(parent_directory, "data")
+csv_filename = "metadata.csv"
+
+# Construct the full save path
+full_save_path = os.path.join(save_directory, csv_filename)
+
+# Check if 'data' folder exists, if not, create it
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
+
+# List directory contents
+directory_contents = os.listdir(directory_path)
 print(f"Contents of directory {directory_path}: {directory_contents}")
 print()
 
+# Collect file metadata
 all_file_metadata = []
 
 for file in directory_contents:
@@ -20,4 +37,14 @@ for file in directory_contents:
         }
         all_file_metadata.append(file_metadata)
 
+# Output collected metadata to console
 print(all_file_metadata)
+
+# Save metadata to CSV
+with open(full_save_path, "w", newline="") as csvfile:
+    csv_writer = csv.DictWriter(
+        csvfile, fieldnames=["file_name", "file_size", "last_modified"]
+    )
+    csv_writer.writeheader()
+    for metadata in all_file_metadata:
+        csv_writer.writerow(metadata)
